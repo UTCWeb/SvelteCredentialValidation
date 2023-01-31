c7e6e53427b6cec9cda1d97e1ce299473eb2bd70
Bernardo Martinez <bernardo-martinez@utc.edu>
2023-01-31 16:20:05 -0500

    UTC-2: Adds a test fetch function.

--- App.svelte
+++ App.svelte
@@ -5,6 +5,13 @@
 	function handleOnSubmit() {
 	  console.log("I'm the handleOnSubmit() in App.svelte");
 	}
+    let characterName;
+
+	fetch('https://swapi.dev/api/people/1')
+		.then((response) => response.json())
+		.then((character) => {
+			characterName = character.name;
+		})
 </script>
 
 <style>
@@ -18,6 +25,7 @@
 	<h1>Hello CodeSandbox</h1>
 	<h2>Start editing to see some magic happen!</h2>
 	<Button />
+    {characterName}
 </main>
 
 <Form on:submit={handleOnSubmit}>

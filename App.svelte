265d9a061ccb0f23eb5da7c5ad410fddae78bf5a
Bernardo Martinez <bernardo-martinez@utc.edu>
2023-01-31 15:54:19 -0500

    UTC-1: Basic 101 Javascript form embed script for a page.

---
+++ App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Button from "./button.svelte";
+	import Form from "./form.svelte";
+
+	function handleOnSubmit() {
+	  console.log("I'm the handleOnSubmit() in App.svelte");
+	}
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button />
+</main>
+
+<Form on:submit={handleOnSubmit}>
+</Form>
---
+++ form.svelte
@@ -0,0 +1,24 @@
+<script>
+	function validate() {
+	  console.log("I'm the validate() function");
+	}
+</script>
+
+<form on:submit|preventDefault={validate}>
+	<input>
+	<button type="submit">
+		Please save me
+	</button>
+  <input>
+	<button type="submit">
+		test 2
+	</button>
+  <input>
+	<button type="submit">
+		test 3 
+	</button>
+  <input>
+	<button type="submit">
+		test 4
+	</button>
+</form>
